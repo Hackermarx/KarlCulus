67a5706956757632d9522eaa0da902ac6eaafef8
Hackermarx <arendvanderrande@hotmail.nl>
2017-12-07 12:17:53 +0100

    Laid basis for function plottng

---
+++ src/main/java/Functions/Plot/FunctionPlot.java
@@ -0,0 +1,12 @@
+package Functions.Plot;
+
+import Functions.Function;
+
+public class FunctionPlot {
+    private int width;
+    private int height;
+
+    public FunctionPlot(Function f) {
+
+    }
+}
---
+++ src/main/java/Functions/Plot/NewtonFractal.java
@@ -0,0 +1,4 @@
+package Functions.Plot;
+
+public class NewtonFractal {
+}
